a6d252876dff35a9fbf904560e526481729e9198
aryan-2206 <aryandoshi22@gmail.com>
2025-09-27 14:07:58 +0530

    tacotron2 model

---
+++ Preprocessing/Text/phoneme_conversion.py
@@ -0,0 +1,11 @@
+from g2p_en import G2p
+from basic_cleaners import metadata
+g2p = G2p()
+
+def g2p_phonemes(text):
+    phonemes = g2p(text)
+    phonemes = [ph for ph in phonemes if ph.isalpha() or ph[:-1].isalpha()]
+    return phonemes
+
+metadata["phonemes"] = metadata["cleaned"].apply(g2p_phonemes)
+print(metadata["phonemes"].head(1))
